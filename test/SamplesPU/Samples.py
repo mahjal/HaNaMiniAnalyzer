2514b0e3acc95687decffd121028fd87bd83830f
mahtab <mahtab.jalal.vandi@cern.ch>
2024-12-01 16:25:45 +0100

    path of root files added

--- test/SamplesPU/Samples.py
+++ test/SamplesPU/Samples.py
@@ -1,11 +1,14 @@
-
 from Haamm.HaNaMiniAnalyzer.Sample import *
 
 import os
+#####
 Sample.WD = os.path.dirname(os.path.abspath(__file__))
+#####
 print( Sample.WD)
 
+#####
 MINIAOD22 = []
+#####
 MINIAOD = []
 #2018
 #ZeroBiasA = Sample("ZeroBiasA" , 0 , False , "/ZeroBias/Run2018A-12Nov2019_UL2018-v2/MINIAOD")
@@ -26,6 +29,7 @@ MINIAOD = []
 #SingleNeutrino_CP1   = Sample("SingleNeutrino_CP1" , 1 , False , "/SingleNeutrino/RunIISummer19UL18MiniAOD-FlatPU0to75_106X_upgrade2018_realistic_v11_L1v1_ext2-v2/MINIAODSIM" )
 #SingleNeutrino_CP5   = Sample("SingleNeutrino_CP5" , 1 , False , "/SingleNeutrino/RunIISummer19UL18MiniAOD-FlatPU0to75_106X_upgrade2018_realistic_v11_L1v1_ext1-v1/MINIAODSIM" )
 
+#####
 SingleNeutrinos = []
 
 
@@ -37,14 +41,13 @@ SingleNeutrinos = []
 #SingleNeutrinos.extend( [ SingleNeutrino_CP1 , SingleNeutrino_CP5 ] )
 #MINIAOD.extend( [ SingleNeutrinoType1, SingleNeutrinoType2, SingleNeutrino_CP1 , SingleNeutrino_CP5 ] )
 #SingleNeutrinos.extend( [ SingleNeutrinoType1, SingleNeutrinoType2, SingleNeutrino_CP1 , SingleNeutrino_CP5 ] )
-
-ZeroBias22A = Sample("ZeroBias22A" , 0 , False , "/ZeroBias/Run2022A-PromptReco-v1/MINIAOD")
-ZeroBias22B = Sample("ZeroBias22B" , 0 , False , "/ZeroBias/Run2022B-PromptReco-v1/MINIAOD")
-ZeroBias22C = Sample("ZeroBias22C" , 0 , False , "/ZeroBias/Run2022C-PromptReco-v1/MINIAOD")
-ZeroBias22D = Sample("ZeroBias22D" , 0 , False , "/ZeroBias/Run2022D-PromptReco-v2/MINIAOD")
-ZeroBias22E = Sample("ZeroBias22E" , 0 , False , "/ZeroBias/Run2022E-PromptReco-v1/MINIAOD")
-ZeroBias22F = Sample("ZeroBias22F" , 0 , False , "/ZeroBias/Run2022F-PromptReco-v1/MINIAOD")
-ZeroBias22G = Sample("ZeroBias22G" , 0 , False , "/ZeroBias/Run2022G-PromptReco-v1/MINIAOD")
+#####ZeroBias22A = Sample("ZeroBias22A" , 0 , False , "/ZeroBias/Run2022A-PromptReco-v1/MINIAOD")
+#####ZeroBias22B = Sample("ZeroBias22B" , 0 , False , "/ZeroBias/Run2022B-PromptReco-v1/MINIAOD")
+#####ZeroBias22C = Sample("ZeroBias22C" , 0 , False , "/ZeroBias/Run2022C-PromptReco-v1/MINIAOD")
+#####ZeroBias22D = Sample("ZeroBias22D" , 0 , False , "/ZeroBias/Run2022D-PromptReco-v2/MINIAOD")
+#####ZeroBias22E = Sample("ZeroBias22E" , 0 , False , "/ZeroBias/Run2022E-PromptReco-v1/MINIAOD")
+#####ZeroBias22F = Sample("ZeroBias22F" , 0 , False , "/ZeroBias/Run2022F-PromptReco-v1/MINIAOD")
+#####ZeroBias22G = Sample("ZeroBias22G" , 0 , False , "/ZeroBias/Run2022G-PromptReco-v1/MINIAOD")
 #SimMiniAod = Sample("SimMiniAod" , 0 , False , "/SingleNeutrino_E-10-gun/Run3Winter22MiniAOD-L1TPU0to99FEVT_SNB_122X_mcRun3_2021_realistic_v9-v2/MINIAODSIM")
 
 
@@ -52,8 +55,10 @@ ZeroBias22G = Sample("ZeroBias22G" , 0 , False , "/ZeroBias/Run2022G-PromptReco-
 
 
 #SimMiniAod = Sample("SimMiniAOD22" , 1 , False , "/SingleNeutrino_E-10-gun/Run3Winter22MiniAOD-L1TPU0to99FEVT_SNB_122X_mcRun3_2021_realistic_v9-v2/MINIAODSIM")
-SimMiniAod = Sample("SimMiniAOD21" , 1 , False , "/SingleNeutrino_E-10-gun/Run3Summer21MiniAOD-SNB_120X_mcRun3_2021_realistic_v5-v2/MINIAODSIM")
-
+######
+SimMiniAod = Sample("SimMiniAOD22" , 1 , False , "/Run3Winter24MiniAOD/SingleNeutrino_E-10-gun/MINIAODSIM/133X_mcRun3_2024_realistic_v9-v3")
+                                                  
+######
 MINIAOD22.extend( [SimMiniAod] )
 
 #SimMiniAod = Sample("SimMiniAOD22" , 1 , False , "/SingleNeutrino_E-10-gun/Run3Winter22MiniAOD-L1TPU0to99FEVT_SNB_122X_mcRun3_2021_realistic_v9-v2/MINIAODSIM")
